5b168c81aa3c590fc5e66bb52ee307264a56cee7
Dik Olga <aolaxy@yandex.ru>
2023-06-13 16:38:38 +0300

    убрала console.log

--- js/functions.js
+++ js/functions.js
@@ -12,11 +12,11 @@ const checkPalindrome = (string) => {
   return totalString === reverseStr;
 };
 //проверка 1 функции
-console.log(controlStringLenght('проверяемая строка', 20));
-console.log(controlStringLenght('проверяемая строка', 18));
-console.log(controlStringLenght('проверяемая строка', 10));
+controlStringLenght('проверяемая строка', 20);
+controlStringLenght('проверяемая строка', 18);
+controlStringLenght('проверяемая строка', 10);
 
 //проверка 2 функции
-console.log(checkPalindrome('топот'));
-console.log(checkPalindrome('Лёша на полке клопа нашёл '));
-console.log(checkPalindrome('кекс'));
+checkPalindrome('топот');
+checkPalindrome('Лёша на полке клопа нашёл ');
+checkPalindrome('кекс');
