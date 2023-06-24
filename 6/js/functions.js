5b0ee8e20aa0ebff86822bd548817aa688595471
Keks <keks@htmlacademy.ru>
2023-06-24 16:59:19 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/functions.js
+++ 6/js/functions.js
@@ -8,12 +8,22 @@ const checkPalindrome = (string) => {
   const reverseStr = revString.reverse().join('');
   return totalString === reverseStr;
 };
-//проверка 1 функции
-controlStringLenght('проверяемая строка', 20);
-controlStringLenght('проверяемая строка', 18);
-controlStringLenght('проверяемая строка', 10);
 
-//проверка 2 функции
-checkPalindrome('топот');
-checkPalindrome('Лёша на полке клопа нашёл ');
-checkPalindrome('кекс');
+controlStringLenght('bcv',3);
+checkPalindrome('njgjn');
+
+const checkTime = (startWorkday,endWorkday,start,proceed)=>{
+  const parseTime = (time)=> {
+    const parts = time.split(':').map(Number);
+    const [hours, minutes] = parts;
+    const minutesPerHour = 60;
+
+    return hours * minutesPerHour + minutes;
+  };
+  return (parseTime(startWorkday) <= parseTime(start) && (parseTime(start) + proceed) <= parseTime(endWorkday));
+};
+checkTime('08:00', '17:30', '14:00', 90);
+checkTime('8:0', '10:0', '8:0', 120);
+checkTime('08:00', '14:30', '14:00', 90);
+checkTime('8:00', '17:30', '08:00', 900);
+
