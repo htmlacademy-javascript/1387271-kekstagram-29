505c7f6bdbfe5e892932a90007ef93b0b0424f14
Dik Olga <aolaxy@yandex.ru>
2023-07-12 14:18:51 +0300

    добавлен stopPropagation и закрытие по Esc

--- js/load-newphoto.js
+++ js/load-newphoto.js
@@ -1,25 +1,40 @@
 //модуль для загрузки нового фото
 import {controlStringLenght } from './util.js';
-const imgUploadInputElement = document.querySelector('.img-upload__input'); //поле загрузки нового фото
-const imgUploadElement = document.querySelector('.img-upload__overlay');//<!-- Форма редактирования изображения -->
+const imgUploadInputField = document.querySelector('.img-upload__input'); //поле загрузки нового фото
+const imgUploadForm = document.querySelector('.img-upload__overlay');//<!-- Форма редактирования изображения -->
 const uploadForm = document.querySelector('.img-upload__form');//Форма отправки данных
 const closeimgButton = document.querySelector('.img-upload__cancel');
+const commentsField = uploadForm.querySelector('.text__description');
 const uploadNewPhoto = ()=>{
-  //изменение поля  для выбора нового фото
-  imgUploadInputElement.addEventListener('change',() => {
-    imgUploadElement.classList.remove('hidden');
+  //отрытие формы для выбора фотографии
+  const openNewPhoto = ()=>{
+    imgUploadForm.classList.remove('hidden');
     document.body.classList.add('modal-open');
-  });
-  //закрытие формы для выбора фотографии
-  closeimgButton.addEventListener('click',()=>{
-    imgUploadElement.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-  });
+    document.addEventListener('keydown',onDocumentKeydown);
+  };
+
   //использование библиотеки Pristine
   const pristine = new Pristine(uploadForm,{
     classTo: 'img-upload__field-wrapper',
     errorTextParent: 'img-upload__field-wrapper'
   });
+  //закрытие формы для выбора фотографии
+  const hideNewPhoto = ()=>{
+    uploadForm.reset();
+    //resetScale();
+    //resetEffects();
+    pristine.reset();
+    imgUploadForm.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+    document.removeEventListener('keydown',onDocumentKeydown);
+  };
+  closeimgButton.addEventListener('click',()=> hideNewPhoto());
+  function onDocumentKeydown(evt){
+    if (evt.key === 'Escape') {
+      //evt.preventDefault();
+      hideNewPhoto();
+    }
+  }
   //проверка комментариев
   const validateComments = (str)=>controlStringLenght(str,140);
   //проверка хэш-тегов
@@ -33,7 +48,7 @@ const uploadNewPhoto = ()=>{
     arrayHashtags.forEach((item) =>(hashtag.test(item)));
   };
   pristine.addValidator(
-    uploadForm.querySelector('.text__description'),
+    commentsField,
     validateComments,
     'Введите от 0 до 140 символов'
   );
@@ -52,7 +67,12 @@ const uploadNewPhoto = ()=>{
     }*/
 
   });
-
-
+  //изменение поля  для выбора нового фото
+  imgUploadInputField.addEventListener('change',() =>openNewPhoto());
+  commentsField.addEventListener('keydown',(evt)=>{
+    if(evt.key === 'Escape'){
+      evt.stopPropagation();
+    }
+  });
 };
 export {uploadNewPhoto};
