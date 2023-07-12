a8630e6e18cbae4a7b6fda6cbbdb5406aa1506c7
Keks <keks@htmlacademy.ru>
2023-07-12 20:40:36 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/util.js
@@ -0,0 +1,24 @@
+//генератор случайного числа в диапозоне:
+const getRandomInteger = (x, y) => {
+  const lower = Math.ceil(Math.min(x, y));
+  const upper = Math.floor(Math.max(x, y));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+//генератор  уникального индентификатора
+const generatorID = () =>{
+  let lastID = 0;
+  return () => {
+    lastID += 1;
+    return lastID;
+
+  };
+};
+const generatorIDComment = generatorID();
+//поиск случайного элемента в переданном массиве
+const getRandomArrElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+// проверка длины строки
+const controlStringLenght = (str,length)=>str.length <= length;
+
+export {getRandomArrElement,generatorIDComment,getRandomInteger,controlStringLenght};
+//
