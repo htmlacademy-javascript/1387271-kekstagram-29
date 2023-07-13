47ed425c68e5e7b5c0b6e7849d10c0f8c715973c
Dik Olga <aolaxy@yandex.ru>
2023-07-13 15:46:20 +0300

    + модуль effect-newphoto с эффектами и слайдером

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import {renderPosts} from './render-posts.js';
 import { uploadNewPhoto } from './load-newphoto.js';
 import { initScaleElement } from './scale.js';
+import {setEffectsSlider} from './effect-newphoto.js';
 renderPosts();
 uploadNewPhoto();
 initScaleElement();
+setEffectsSlider();
 
