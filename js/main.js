a54f6183a67e969027056798a1e69b225614549e
Dik Olga <aolaxy@yandex.ru>
2023-07-15 12:34:35 +0300

    загружаются кроме комментариев

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import {renderPosts} from './render-posts.js';
 import { uploadNewPhoto } from './load-newphoto.js';
 import { initScaleElement } from './scale.js';
 import {setEffectsSlider} from './effect-newphoto.js';
-import { getData, sendData } from './api.js';
+import { getData, sendData } from './data-api.js';
 /*setOnFormSubmit(async (data) => {
   try {
     await sendData(data);
