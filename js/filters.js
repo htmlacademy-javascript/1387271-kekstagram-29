c445cb9e3f518159883e7308f74061923c9c3866
Dik Olga <aolaxy@yandex.ru>
2023-07-18 22:19:54 +0300

    добавлены filters, не сделано

--- js/filters.js
+++ js/filters.js
@@ -1,2 +1,27 @@
-//import { renderPosts } from "./render-posts";
-//import{}
+import { getRandomInteger } from "./util";
+const COUNT_PICTURES = 10;
+const Filters = {
+  DEFAULT:'filter-default',
+  RANDOM:'filter-random',
+  POPULAR:'filter-discussed',
+};
+let images = [];
+const filterElement = document.querySelector('.img-filters');
+let presentFilter = Filters.DEFAULT;
+const sortByComments = (img1,img2)=> img2.comments.length - img1.comments.length;
+const getSortedImages = (images,cb)=>{
+  switch(presentFilter){
+    case Filters.RANDOM:
+      return images.slice().sort(getRandomInteger(1,COUNT_PICTURES));
+    case Filters.POPULAR:
+      return images.slice().sort(sortByComments);
+    default:
+      return images.slice();
+  }
+const setOnFilterClick=(cb)=>{
+
+
+}
+}
+
+
