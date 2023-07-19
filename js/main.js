1135f3803634f6661c24f41f739a1ec03da4e909
Dik Olga <aolaxy@yandex.ru>
2023-07-19 13:15:40 +0300

    добавлена загрузка фото

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ import {renderPosts} from './render-posts.js';
 import { initScaleElement } from './scale.js';
 import {setEffectsSlider} from './effect-newphoto.js';
 import { getData, sendData } from './data-api.js';
-import { hideNewPhoto,installForm,setOnFormSubmit,unblockSubmitButton} from './load-newphoto.js';
+import { hideNewPhoto,installForm,setOnFormSubmit,unblockSubmitButton} from './form-newphoto.js';
 import { showMessage} from './util.js';
 import { showSuccessMessage,showErrorMessage } from './messages.js';
 import {setDebouncedFilter, showFilters} from './filters.js';
+import { setPreviewPictureLoader } from './upload-newphoto.js';
 setOnFormSubmit(async (data) => {
   try {
     await sendData(data);
@@ -25,6 +26,7 @@ try {
   initScaleElement();
   setEffectsSlider();
   showFilters();
+  setPreviewPictureLoader();
   setDebouncedFilter(data);
 } catch (err) {
   showMessage(err.message);
